21b09f6156d0132362509c3785fe09215bd0b1fc
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-04-29 09:45:14 +0200

    update baza
    zmiana bazy na lokalną

--- baza.php
+++ baza.php
@@ -1,11 +1,22 @@
 <?php
-
+/*
 $host="userdb1";
 $login_db="1225338_8xx4t4";
 $password_db="s3zHrIgXJEejfX";
 $base="1225338_8xx4t4";
+*/
+
+
+$host="localhost";
+$login_db="root";
+$password_db="";
+$base="motoryzacja";
+
+
 $conn = new mysqli($host,$login_db,$password_db,$base);
   
+
+
 //$d = "mysql:host=localhost;db_name=motoryzacja";
 
 //$conn = new PDO($d, $login_db, $password_db);
